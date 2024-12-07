8be1253b3a8cbac2c5d4d788bb374bdc0492d0d5
David <davidthemaster30@hotmail.com>
2024-12-06 23:11:32 -0500

    update script to include missing required files

--- make_release.sh
+++ make_release.sh
@@ -3,4 +3,5 @@ rm -rf bin
 
 dotnet build RF5.HisaCat.NPCDetails.csproj -f net6.0 -c Release
 
-zip -j 'RF5.HisaCat.NPCDetails_v1.5.0.zip' './bin/Release/net6.0/RF5.HisaCat.NPCDetails.dll' './RF5.HisaCat.NPCDetails'
+zip -j 'RF5.HisaCat.NPCDetails_v1.5.1.zip' './bin/Release/net6.0/RF5.HisaCat.NPCDetails.dll' 
+zip -ur 'RF5.HisaCat.NPCDetails_v1.5.1.zip' './RF5.HisaCat.NPCDetails'
